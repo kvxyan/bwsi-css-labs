d25c2909f2c9d3245dcb41327d71f0950f128b36
kvxyan <kevinyan2008@yahoo.com>
2026-03-29 19:10:57 -0400

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Kevin Yan" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone! My name is Kevin Yan, and I’m from upstate New York. In my research, I independently designed and built an oral surgical robotic system that combines a six-DOF robotic arm, a six-axis force/torque sensor with a novel hybrid elastomer structure, and AI-based 3D reconstruction.")
 
 if __name__ == "__main__":
     main()
